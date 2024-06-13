87ba86b7ce58ebba6ac317d39fbdf83f872243b8
Tarek Messaoudi <tmessaoudi94@gmail.com>
2024-06-14 00:00:28 +0200

    Remove Jokes api route

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -5,7 +5,6 @@
 import * as $_404 from "./routes/_404.tsx";
 import * as $_app from "./routes/_app.tsx";
 import * as $_layout from "./routes/_layout.tsx";
-import * as $api_joke from "./routes/api/joke.ts";
 import * as $index from "./routes/index.tsx";
 import * as $resume_english from "./routes/resume/english.tsx";
 import * as $Counter from "./islands/Counter.tsx";
@@ -17,7 +16,6 @@ const manifest = {
     "./routes/_404.tsx": $_404,
     "./routes/_app.tsx": $_app,
     "./routes/_layout.tsx": $_layout,
-    "./routes/api/joke.ts": $api_joke,
     "./routes/index.tsx": $index,
     "./routes/resume/english.tsx": $resume_english,
   },
